393f641c51aa50846e41c11984e4ad495478befb
ayencha1412-ux <ayencha1412@gmail.com>
2026-09-02 22:36:02 +0800

    Remove obsolete low quality March 10 media bundle

--- src/data/march10Sprite.js
+++
@@ -1,9 +0,0 @@
-import chunk1 from './march10SpriteChunk1.js';
-import chunk2 from './march10SpriteChunk2.js';
-import chunk3 from './march10SpriteChunk3.js';
-import chunk4 from './march10SpriteChunk4.js';
-import chunk5 from './march10SpriteChunk5.js';
-import chunk6 from './march10SpriteChunk6.js';
-import chunk7 from './march10SpriteChunk7.js';
-
-export const march10Sprite = `data:image/webp;base64,${chunk1}${chunk2}${chunk3}${chunk4}${chunk5}${chunk6}${chunk7}`;
