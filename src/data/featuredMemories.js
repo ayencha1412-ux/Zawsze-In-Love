25be8ca26ba1cfb4d97c62a19b190497c84be89a
ayencha1412-ux <ayencha1412@gmail.com>
2026-09-03 03:11:56 +0800

    Extract featured memory data

---
+++ src/data/featuredMemories.js
@@ -0,0 +1,23 @@
+import photo1 from '../assets/photo-1.webp';
+import photo2 from '../assets/photo-2.webp';
+import photo3 from '../assets/photo-3.webp';
+
+const FEATURED_MEMORIES = [
+  {
+    src: photo1,
+    caption: 'that one afternoon',
+    note: 'a day worth keeping',
+  },
+  {
+    src: photo2,
+    caption: 'still my favorite',
+    note: 'in every version of us',
+  },
+  {
+    src: photo3,
+    caption: 'just because',
+    note: 'the ordinary became ours',
+  },
+];
+
+export default FEATURED_MEMORIES;
